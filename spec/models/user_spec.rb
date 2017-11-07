b969ad209b78b9730d4b0ba0218ecb64e2fb5068
mattMusser <matthew.musser@gmail.com>
2017-11-07 16:53:39 -0600

    Checkpoint 23 Finished Again

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "PostEnvy User", email: "user@postenvy.com", password: "password") }
+  let(:user) { User.create!(name: "Postenvy User", email: "user@postenvy.com", password: "password") }
   # Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -19,13 +19,13 @@ RSpec.describe User, type: :model do
 
   describe "attributes" do
     it "should have name and email attributes" do
-      expect(user).to have_attributes(name: "PostEnvy User", email: "user@postenvy.com")
+      expect(user).to have_attributes(name: "Postenvy User", email: "user@postenvy.com")
     end
   end
 
   describe "invalid user" do
     let(:user_with_invalid_name) { User.new(name: "", email: "user@postenvy.com") }
-    let(:user_with_invalid_email) { User.new(name: "PostEnvy User", email: "") }
+    let(:user_with_invalid_email) { User.new(name: "Postenvy User", email: "") }
 
     it "should be an invalid user due to blank name" do
       expect(user_with_invalid_name).to_not be_valid
