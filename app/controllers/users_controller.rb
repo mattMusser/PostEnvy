7c27d3d2653611e5aa5be2a0bea753ecffdcd92f
mattMusser <matthew.musser@gmail.com>
2017-11-11 17:57:20 -0600

    Checkpoint 24 still a WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def create
+	# Create a new user with new and set the corresponding attributes from the params hash.
+		@user = User.new
+		@user.name = params[:user][:name]
+		@user.email = params[:user][:email]
+		@user.password = params[:user][:password]
+		@user.password_confirmation = params[:user][:password_confirmation]
+	# Save the new user to the db. If the db save is successful, add a flash message and then redirect the user to the root path
+		if @user.save
+			flash[:notice] = "Welcome to PostEnvy #{@user.name}!"
+			redirect_to root_path
+	# Otherwise, display an error and render the new view.
+		else
+			flash.now[:alert] = "There was an error creating your account. Please try again."
+			render :new
+		end
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,14 +45,6 @@ ActiveRecord::Schema.define(version: 20171107172205) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "topics", force: :cascade do |t|
     t.string "name"
     t.boolean "public", default: true
