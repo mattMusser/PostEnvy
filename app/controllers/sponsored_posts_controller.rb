d7416aa3c1bef9712a9c8375a61fc1da10f1d3b8
mattMusser <matthew.musser@gmail.com>
2017-11-16 12:27:12 -0600

    Assignment 21 Finally Finished I hope

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -11,6 +11,7 @@ class SponsoredPostsController < ApplicationController
   def create
     @sponsored = SponsoredPost.new
     @sponsored.title = params[:sponsored_post][:title]
+    @sponsored.price = params[:sponsored_post][:price]
     @sponsored.body = params[:sponsored_post][:body]
     @topic = Topic.find(params[:topic_id])
   # Assigns a topic to a sponsored post
@@ -27,5 +28,20 @@ class SponsoredPostsController < ApplicationController
 
   def edit
     @sponsored = SponsoredPost.find(params[:id])
+  end
+
+  def update
+    @sponsored = SponsoredPost.find(params[:id])
+    @sponsored.title = params[:sponsored_post][:title]
+    @sponsored.price = params[:sponsored_post][:price]
+    @sponsored.body = params[:sponsored_post][:body]
+
+    if @sponsored.save
+      flash[:notice] = "Post was updated."
+      redirect_to [@sponsored.topic, @sponsored]
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
   end
 end
