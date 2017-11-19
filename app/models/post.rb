6c67512b01e96682e386d8657db9c0d804f9d075
mattMusser <matthew.musser@gmail.com>
2017-11-19 14:28:38 -0600

    Assignment 26 Finsihed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
 	has_many :comments, dependent: :destroy
 # Orders all posts by their created_at date, in descending order, with the most recent posts displayed first.
 	default_scope { order('created_at DESC') }
+# Orders posts by :title
+	scope :ordered_by_title, -> { order('title DESC') }
+# Reverse order by created date
+	scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 
 	validates :title, length: { minimum: 5 }, presence: true
 	validates :body, length: { minimum: 20 }, presence: true
