849c40eda6b48a9e38c15dbf86cc2d906bad1208
mattMusser <matthew.musser@gmail.com>
2017-11-22 20:52:31 -0600

    Checkpoint 28 Finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
+# Used [] so /posts/:id routes aren't created, just posts/:post_id/comments routes.
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
