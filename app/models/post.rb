84a502b91b17fa5562d035d73792d3f5d9212fb7
mattMusser <matthew.musser@gmail.com>
2017-11-24 15:29:19 -0600

    Assignment 29 WIP

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 	has_many :votes, dependent: :destroy
+	after_create :create_vote
+
 # Orders all posts by their created_at date, in descending order, with the most recent posts displayed first.
 	default_scope { order('rank DESC') }
 
@@ -29,5 +31,11 @@ class Post < ApplicationRecord
 		age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
 		new_rank = points + age_in_days
 		update_attribute(:rank, new_rank)
+	end
+
+	private
+
+	def create_vote
+		user.votes.create(value: 1, post: self)
 	end
 end
